4d60cbd9f2c79922b7aa6042191e13fbef11d38b
Sedigheh Farasat <sd.farasat@gmail.com>
2022-04-11 14:17:08 +0430

    Added Url routing and views

---
+++ blog_project/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+
+    class Meta:
+        ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'post'
