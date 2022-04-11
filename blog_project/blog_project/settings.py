3573035e7abfe5bd9c7415b7c9f401307ebaa01f
Sedigheh <sd.farasat@gmail.com>
2022-04-11 19:22:16 +0430

    Added requirements.txt

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
