fdfc3274caa321ceff641e2bf126bb7d928e42f1
Sedigheh <sd.farasat@gmail.com>
2022-04-15 17:18:57 +0430

    Fixed a bug

--- blog_project/users/views.py
+++ blog_project/users/views.py
@@ -9,9 +9,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are able to login now!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
