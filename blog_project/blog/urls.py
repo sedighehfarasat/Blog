68f64abd17b7d739b894c9e11f3fce44c196770f
Sedigheh <sd.farasat@gmail.com>
2022-05-04 17:30:29 +0430

    Added 'delete post'

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='blog-detail'),
     path('post/new/', views.PostCreateView.as_view(), name='blog-create'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='blog-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='blog-delete'),
 ]
